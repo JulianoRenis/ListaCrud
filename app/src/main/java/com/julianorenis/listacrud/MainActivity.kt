015bedbbc783cb78dd79716904dbf03bab17aa29
JULIANO RENIS GOMES SILVA DOS SANTOS <julianorenis@gmail.com>
2023-01-24 22:14:47 -0300

    Criando o layout de adicionar item implementando navigation e criando pack para navigation craindo toolbar

--- app/src/main/java/com/julianorenis/listacrud/MainActivity.kt
+++ app/src/main/java/com/julianorenis/listacrud/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.julianorenis.listacrud
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tool: Toolbar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tool = findViewById(R.id.app_toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        tool.setupWithNavController(navController, appBarConfiguration)
     }
 }
